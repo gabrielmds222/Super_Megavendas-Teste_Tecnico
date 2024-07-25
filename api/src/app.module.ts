0ba24a1224fad955e33d98e3b2c84d597c234232
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-07-25 10:52:06 -0300

    feat: Instalacao de dependencias

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    AuthModule,
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
